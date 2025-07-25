3273b1b68b79fdf449c93c662f912a2224a6ff05
uforco <srka780@gmail.com>
2025-07-26 01:59:49 +0600

    server test up cookies 666

---
+++ src/app/callback/google/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>loading... save cookie</div>;
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,18 @@ import { useEffect } from "react";
 
 export default function Home() {
   function handleLogin() {
-    // window.location.href = "https://prechserver.zapto.org/auth/google";
-    document.cookie = `accessToken2=${"sharif"}; path=/; max-age=3600; secure; samesite=strict`;
+    window.location.href = "http://localhost:3000/auth/google";
   }
 
   const handleCookie = async () => {
-    console.log("sent cookie proscee back end")
-    await fetch("https://prechserver.zapto.org/auth/me", {
-      method: "GET",
-      credentials: "include",
+    console.log("sent cookie proscee back end");
+    await fetch("http://localhost:3000/auth/me", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify({ refresh_token: "" }),
+      body: JSON.stringify({ refresh_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZhYmM0ZTlhLTVlZDMtNDVkYy04M2M1LTNhYjFhN2MwYWMyMyIsImVtYWlsIjoidWZvcmNvZGUxMjNAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJwcm92aWRlciI6Imdvb2dsZSIsImlhdCI6MTc1MzQ3MjU1OCwiZXhwIjoxNzU0MDc3MzU4fQ.3SK5hDif9FMGxhwBL3jKyyr5NWc0m90QwOGH8VdtYxk" }),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -21,6 +24,9 @@ export default function Home() {
       .catch((error) => {
         console.error("Error:", error);
       });
+  };
+  const showCookie = async () => {
+    console.log(document.cookie);
   };
 
   return (
@@ -36,6 +42,12 @@ export default function Home() {
         className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
       >
         sent cookie
+      </button>
+      <button
+        onClick={showCookie}
+        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+      >
+        show cookie
       </button>
     </div>
   );
