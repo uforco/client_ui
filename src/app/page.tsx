dd98e4704bdbea2af96bf3ddf3019863e9c12635
uforco <srka780@gmail.com>
2025-07-25 23:09:41 +0600

    https ssl set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,29 @@ import { useEffect } from "react";
 
 export default function Home() {
   function handleLogin() {
-    window.location.href = "http://prechserver.zapto.org/auth/google";
+    window.location.href = "https://prechserver.zapto.org/auth/google";
     // window.location.href = "http://localhost:3000/auth/google";
   }
 
   // const searchParams = useSearchParams();
   // const token = searchParams.get("urlaccess");
   // // console.log("Token:===========5684568===", token);
-  useEffect(() => {
-    fetch("http://prechserver.zapto.org/auth/me", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include", // Required to send/receive cookies
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Data:===========", data);
-      })
-      .catch((err) => {
-        console.log("Error:===========", err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://prechserver.zapto.org/auth/me", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     credentials: "include", // Required to send/receive cookies
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log("Data:===========", data);
+  //     })
+  //     .catch((err) => {
+  //       console.log("Error:===========", err);
+  //     });
+  // }, []);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
