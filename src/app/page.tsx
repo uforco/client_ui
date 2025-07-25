6d2ce36c38b8ce1ca21af7473f1049061c919cac
uforco <srka780@gmail.com>
2025-07-25 23:01:18 +0600

    xxx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Home() {
   function handleLogin() {
     window.location.href = "http://prechserver.zapto.org/auth/google";
     // window.location.href = "http://localhost:3000/auth/google";
   }
+
+  // const searchParams = useSearchParams();
+  // const token = searchParams.get("urlaccess");
+  // // console.log("Token:===========5684568===", token);
+  useEffect(() => {
+    fetch("http://prechserver.zapto.org/auth/me", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Required to send/receive cookies
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Data:===========", data);
+      })
+      .catch((err) => {
+        console.log("Error:===========", err);
+      });
+  }, []);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
